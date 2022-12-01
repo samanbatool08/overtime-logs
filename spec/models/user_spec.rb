ec61a2e911b773bc04d07404fc50d6de701c7d86
Saman Batool <samanbatool08@gmail.com>
2022-12-01 18:45:59 -0500

    implemented full name method for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do 
+    @user = User.create(first_name: 'Saman', last_name: 'Batool', email: "testuser@test.com", password: 'asdfasdf', password_confirmation: 'asdfasdf')
+  end
   describe 'creation' do
-    before do 
-      @user = User.create(first_name: 'Saman', last_name: 'Batool', email: "testuser@test.com", password: 'asdfasdf', password_confirmation: 'asdfasdf')
-    end
-
     it 'can be created' do 
       expect(@user).to be_valid
     end
@@ -15,6 +14,11 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
+  end
 
+  describe "custom methods" do 
+    it 'has a full name method that combines first and last name' do 
+      expect(@user.full_name).to eq("BATOOL, SAMAN")
+    end 
   end
 end
